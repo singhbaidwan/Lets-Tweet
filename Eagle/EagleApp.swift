e83af9a8667a7cdd71ad3f605feecafa779da27c
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-06-13 22:08:38 +0530

    Initial Commit

---
+++ Eagle/EagleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EagleApp.swift
+//  Eagle
+//
+//  Created by Dalveer singh on 13/06/22.
+//
+
+import SwiftUI
+
+@main
+struct EagleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
