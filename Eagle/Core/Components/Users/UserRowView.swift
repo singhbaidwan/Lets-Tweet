8a1822f8c01e9146b36d99756eebad7001115677
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-06-21 21:38:08 +0530

    Search Bar done

--- Eagle/Core/Components/Users/UserRowView.swift
+++ Eagle/Core/Components/Users/UserRowView.swift
@@ -6,17 +6,21 @@
 //
 
 import SwiftUI
-
+import Kingfisher
 struct UserRowView: View {
+    let user:User
     var body: some View {
         HStack(spacing:12){
-            Circle()
-                .frame(width: 48, height: 48)
+            KFImage(URL(string: user.profileImageUrl))
+                .resizable()
+                .clipShape(Circle())
+                .scaledToFill()
+                .frame(width: 56, height: 56)
             VStack(alignment : .leading,spacing: 4){
-                Text("Dalveer Singh")
+                Text(user.username)
                     .font(.subheadline).bold()
                     .foregroundColor(.black)
-                Text("DBS")
+                Text(user.fullname)
                     .font(.subheadline)
                     .foregroundColor(.gray)
             }
@@ -28,8 +32,8 @@ struct UserRowView: View {
     
 }
 
-struct UserRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        UserRowView()
-    }
-}
+//struct UserRowView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        UserRowView()
+//    }
+//}
---
+++ Eagle/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  ExploreViewModel.swift
+//  Eagle
+//
+//  Created by Dalveer singh on 21/06/22.
+//
+
+import Foundation
+class ExploreViewModel:ObservableObject{
+    @Published var users = [User]()
+    @Published var searchText=""
+    var searchableUsers:[User]{
+        if searchText.isEmpty{
+            return users
+        }
+        else{
+            let lowercaseQuery = searchText.lowercased()
+            return users.filter({
+                $0.username.contains(lowercaseQuery) ||
+                $0.fullname.lowercased().contains(lowercaseQuery)
+            })
+        }
+    }
+    let service =  UserService()
+    init(){
+        fetchUsers()
+    }
+    func fetchUsers(){
+        service.fetchUsers { users in
+            self.users = users
+            print("DEBUG : Users are \(users)")
+        }
+    }
+}
